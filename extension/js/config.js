f047bddb15d8473aaa9e2193faae1678865df93f
Nikolas Karalis <nikolaskaralis@gmail.com>
2026-08-18 19:17:14 +0200

    Configs

--- extension/js/config.js
+++ extension/js/config.js
@@ -4,7 +4,7 @@ export const NOTION_VERSION = "2026-03-11";
 
 // For a private/local install, you can either paste the deployed Worker URL in
 // the onboarding screen, or hard-code it here before loading the extension.
-export const DEFAULT_OAUTH_BACKEND = "";
+export const DEFAULT_OAUTH_BACKEND = "https://tabs2notion-oauth.nikolaskaralis.workers.dev";
 
 export const MAX_DATABASES_TO_LOAD = 500;
 export const CREATE_REQUEST_SPACING_MS = 380;
